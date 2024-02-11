caf0534ede2e0e3d25663cb12ea290852493904f
wisekim <lwisekiml@naver.com>
2024-02-11 22:19:17 +0900

    calculateGradeTest(), GradeCalculator class

--- oop-pratice/src/test/java/org/example/grade/GradeCalculatorTest.java
+++ oop-pratice/src/test/java/org/example/grade/GradeCalculatorTest.java
@@ -1,5 +1,6 @@
 package org.example.grade;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -18,8 +19,14 @@ public class GradeCalculatorTest {
     // 이수한 과목을 전달하여 평균학점 계산 요청 ---> 학점 계산기 ---> (학점수x교과목 평점)의 합계 ---> 과목(코스)
     //                                                    ---> 수상신창 총학점 수          ---> 과목(코스)
 
+    @DisplayName("평균 학점을 계산한다.")
     @Test
     void calculateGradeTest() {
+        List<Course> courses = List.of(new Course("OOP", 3, "A+"), new Course("자료구조", 3, "A+"));
 
+        GradeCalculator gradeCalculator = new GradeCalculator(courses);
+        double gradeResult = gradeCalculator.calculateGrade();
+
+        assertThat(gradeResult).isEqualTo(4.5);
     }
 }
