67c571587c65965a297f97dadfb56352716801b1
wisekim <lwisekiml@naver.com>
2024-02-11 23:01:12 +0900

    refactoring

--- oop-pratice/src/main/java/org/example/grade/GradeCalculator.java
+++ oop-pratice/src/main/java/org/example/grade/GradeCalculator.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class GradeCalculator {
 
-    private final List<Course> courses;
+    private final Courses courses;
 
     public GradeCalculator(List<Course> courses) {
-        this.courses = courses;
+        this.courses = new Courses(courses);
     }
 
     /**
@@ -17,15 +17,10 @@ public class GradeCalculator {
      */
     public double calculateGrade() {
         // (학점수x교과목 평점)의 합계
-        double multipliedCreditAndCourseGrade = 0;
-        for (Course course : courses) {
-            multipliedCreditAndCourseGrade += course.multiplyCreditAndCourseGrade();
-        }
+        double multipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
 
         // 수강신청 총학점 수
-        int totalCompletedCredit = courses.stream()
-                .mapToInt(Course::getCredit) // course -> course.getCredit()
-                .sum();
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
 
         return multipliedCreditAndCourseGrade / totalCompletedCredit;
     }
